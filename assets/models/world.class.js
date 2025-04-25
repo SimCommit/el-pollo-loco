82014a58f6d19a4a9bef9c2841bf678904bde393
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-04-25 13:42:52 +0200

    added class diagram

--- assets/models/world.class.js
+++ assets/models/world.class.js
@@ -1,23 +1,29 @@
 // world.class.js
 
 class World {
-  clouds = [new Cloud()];
   backgroundObjects = [
+    new BackgroundObject("../assets/img/5_background/layers/air.png", -719),
+    new BackgroundObject("../assets/img/5_background/layers/3_third_layer/2.png", -719),
+    new BackgroundObject("../assets/img/5_background/layers/2_second_layer/2.png", -719),
+    new BackgroundObject("../assets/img/5_background/layers/1_first_layer/2.png", -719),
     new BackgroundObject("../assets/img/5_background/layers/air.png", 0),
     new BackgroundObject("../assets/img/5_background/layers/3_third_layer/1.png", 0),
     new BackgroundObject("../assets/img/5_background/layers/2_second_layer/1.png", 0),
     new BackgroundObject("../assets/img/5_background/layers/1_first_layer/1.png", 0),
   ];
+  clouds = [new Cloud()];
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   canvas;
   ctx;
   keyboard;
+  camera_x = 0;
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d"); // ???
     this.canvas = canvas;
     this.keyboard = keyboard;
+    this.extendBackgroundObjects();
     this.draw();
     this.setWorld();
   }
@@ -26,16 +32,36 @@ class World {
     this.character.world = this;
   }
 
+  extendBackgroundObjects() {
+    for (let i = 1; i < 3; i++) {
+      this.factor = i * 719;
+      this.backgroundObjects.push(
+        new BackgroundObject("../assets/img/5_background/layers/air.png", 0 + this.factor),
+        new BackgroundObject("../assets/img/5_background/layers/3_third_layer/2.png", 0 + this.factor),
+        new BackgroundObject("../assets/img/5_background/layers/2_second_layer/2.png", 0 + this.factor),
+        new BackgroundObject("../assets/img/5_background/layers/1_first_layer/2.png", 0 + this.factor),
+        new BackgroundObject("../assets/img/5_background/layers/air.png", 719 + this.factor),
+        new BackgroundObject("../assets/img/5_background/layers/3_third_layer/1.png", 719 + this.factor),
+        new BackgroundObject("../assets/img/5_background/layers/2_second_layer/1.png", 719 + this.factor),
+        new BackgroundObject("../assets/img/5_background/layers/1_first_layer/1.png", 719 + this.factor),
+      );
+    }
+  }
+
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.ctx.translate(this.camera_x, 0);
 
     this.addObjectsToMap(this.backgroundObjects);
     this.addObjectsToMap(this.clouds);
     this.addToMap(this.character);
     this.addObjectsToMap(this.enemies);
 
+    this.ctx.translate(-this.camera_x, 0);
+
     // The function is repeatedly called via requestAnimationFrame
-    requestAnimationFrame( () => {
+    requestAnimationFrame(() => {
       this.draw();
     });
 
@@ -66,12 +92,12 @@ class World {
 
   flipImage(mo) {
     this.ctx.save();
-    this.ctx.translate(mo.width, 0);  //  shift by object width to prevent "teleporting" when flipping (otherDirection)
-    this.ctx.scale(-1, 1);  
+    this.ctx.translate(mo.width, 0); //  shift by object width to prevent "teleporting" when flipping (otherDirection)
+    this.ctx.scale(-1, 1);
     mo.x = mo.x * -1;
   }
 
-  flipImageBack(mo){
+  flipImageBack(mo) {
     mo.x = mo.x * -1;
     this.ctx.restore();
   }
